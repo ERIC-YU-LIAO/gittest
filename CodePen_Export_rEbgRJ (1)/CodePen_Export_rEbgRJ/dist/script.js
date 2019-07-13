c53128082d1842c9adb3a638f0c992af4d483bd2
ERIC-YU-LIAO <40856536+ERIC-YU-LIAO@users.noreply.github.com>
2019-07-13 18:27:40 +0800

    練習
    練習

---
+++ CodePen_Export_rEbgRJ (1)/CodePen_Export_rEbgRJ/dist/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+  $(".item1").hover(function(){
+    $(".title1").css("font-weight","900");
+    $(".item1").animate({
+      top: "-8px"
+    });
+  $(".item1").mouseleave(function(){
+    $(".title1").css("font-weight","300");
+    $(".item1").animate({
+      top: "0px"
+    },300);
+  });
+  });
+});
+
+
+$(document).ready(function(){
+  $(".item2").hover(function(){
+    $(".title2").css("font-weight","900");
+    $(".item2").animate({
+      top: "-8px"
+    });
+  $(".item2").mouseleave(function(){
+    $(".title2").css("font-weight","300");
+    $(".item2").animate({
+      top: "0px"
+    },300);
+  });
+  });
+});
+
+
+$(document).ready(function(){
+  $(".item3").hover(function(){
+    $(".title3").css("font-weight","900");
+    $(".item3").animate({
+      top: "-8px"
+    });
+  $(".item3").mouseleave(function(){
+    $(".title3").css("font-weight","300");
+    $(".item3").animate({
+      top: "0px"
+    },300);
+  });
+  });
+});
+
+
+
+$(document).ready(function(){
+  $(".item4").hover(function(){
+    $(".title4").css("font-weight","900");
+    $(".item4").animate({
+      top: "-8px"
+    });
+  $(".item4").mouseleave(function(){
+    $(".title4").css("font-weight","300");
+    $(".item4").animate({
+      top: "0px"
+    },300);
+  });
+  });
+});
